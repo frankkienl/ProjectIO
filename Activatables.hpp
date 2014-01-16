edcdb5edc5c4c1a9ac4724ce5c0207c7023ffa2a
Andrew Dionne <andrewrdionne@gmail.com>
2014-01-15 21:41:18 -0500

    - Initial starting of Maze class. - Addition of missing license stuffs.

--- Activatables.hpp
+++ Activatables.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Cepheid
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
 #ifndef ActivatablesHPP
 #define ActivatablesHPP
 
@@ -153,6 +168,36 @@ namespace io {
     }
   private:
     SecretDoorDirection direction;
+  };
+
+  enum class StairDirection {
+    UP,
+    DOWN
+  };
+
+  class Stairs : public Activatable {
+  public:
+    Stairs() {
+      setDirection(StairDirection::UP);
+    }
+
+    virtual ~Stairs() {
+
+    }
+
+    virtual void activate(Entity* activator) {
+
+    }
+
+    StairDirection getDirection() const {
+      return direction;
+    }
+
+    void setDirection(const StairDirection direction) {
+      this->direction = direction;
+    }
+  private:
+    StairDirection direction;
   };
 }
 
